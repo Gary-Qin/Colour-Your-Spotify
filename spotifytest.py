702d00f29c4b3c64c51fe19e1780c8416fce5f64
Gary Qin <garyzhqin@gmail.com>
2023-01-14 17:36:52 -0500

    real

--- spotifytest.py
+++ spotifytest.py
@@ -1,18 +1,18 @@
-import os
 import sys
-import json
 import spotipy
-import webbrowser
 import spotipy.util as util
-from json.decoder import JSONDecodeError
-
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-library-read"
+# $env:SPOTIPY_CLIENT_ID='94999ad051a24821989fc2b351ece004'
+# $env:SPOTIPY_CLIENT_SECRET='a642a27e5fd04cbe80e6512c41ae804a'
+# $env:SPOTIPY_REDIRECT_URI='http://google.com/'
 
+scope = "user-library-read user-modify-playback-state user-read-playback-state"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_saved_tracks()
+results = sp.current_user_saved_tracks(10, 0)
 for idx, item in enumerate(results['items']):
     track = item['track']
     print(idx, track['artists'][0]['name'], " – ", track['name'])
+# sp.volume(100)
+print(sp.devices())
