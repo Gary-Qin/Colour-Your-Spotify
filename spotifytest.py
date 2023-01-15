9ee4597801716bbc0eb7fd57a8da0d3c4ca46bed
Gary Qin <garyzhqin@gmail.com>
2023-01-14 19:54:07 -0500

    asgasdg

--- spotifytest.py
+++ spotifytest.py
@@ -8,13 +8,20 @@ from spotipy.oauth2 import SpotifyOAuth
 # $env:SPOTIPY_REDIRECT_URI='http://google.com/'
 
 scope = "user-library-read user-modify-playback-state user-read-playback-state"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="94999ad051a24821989fc2b351ece004",
+                                               client_secret="a642a27e5fd04cbe80e6512c41ae804a",
+                                               redirect_uri="http://google.com/",
+                                               scope=scope))
+
+                                        
 
 # results = sp.current_user_saved_tracks(10, 0)
 # for idx, item in enumerate(results['items']):
 #     track = item['track']
 #     print(idx, track['artists'][0]['name'], " – ", track['name'])
 sp.volume(100)
-sp.add_to_queue("6Hmc5Afutf4hcrjnBYNidS?si=78d5396df4e644d4")
-sp.next_track()
-sp.seek_track(85000)
+sp.add_to_queue("6Hmc5Afutf4hcrjnBYNidS?")
+# sp.next_track()
