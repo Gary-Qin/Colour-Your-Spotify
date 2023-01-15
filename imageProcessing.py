567abbb64c8d4e52f97b6707e7bce794a4dff744
MFactor1 <mingsqu@gmail.com>
2023-01-14 20:46:57 -0500

    Update imageProcessing.py

--- imageProcessing.py
+++ imageProcessing.py
@@ -4,5 +4,7 @@ import numpy as np
 def fetch_image():
     pass
 
-def process_image(image):
-    img_matrix = imread()
+def process_image(img_matrix):
+    av_color_row = np.average(img_matrix, axis=0)
+    av_color = np.average(av_color_row, axis=0)
+    print(av_color)
