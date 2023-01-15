6b6e707a88eeec7cac9dd929896763f9efbfb0b3
MFactor1 <mingsqu@gmail.com>
2023-01-14 22:50:29 -0500

    Updated spotifytest.py

--- spotifytest.py
+++ spotifytest.py
@@ -3,23 +3,37 @@ import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
 
-# $env:SPOTIPY_CLIENT_ID='94999ad051a24821989fc2b351ece004'
-# $env:SPOTIPY_CLIENT_SECRET='a642a27e5fd04cbe80e6512c41ae804a'
-# $env:SPOTIPY_REDIRECT_URI='http://google.com/'
 
-scope = "user-library-read user-modify-playback-state user-read-playback-state"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="94999ad051a24821989fc2b351ece004",
-                                               client_secret="a642a27e5fd04cbe80e6512c41ae804a",
-                                               redirect_uri="http://google.com/",
-                                               scope=scope))
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="3a90ccaaa2ed467ca3446c605016e52b",
+                                               client_secret="f07b311374724e16b0629a80aba4bd7b",
+                                               redirect_uri="http://localhost:3000",
+                                               scope="user-library-read user-modify-playback-state user-read-playback-state"))
 
+playlist_id = 'https://open.spotify.com/playlist/0tnsqKhnn7rKAD7sevyRU2?si=b207536b74584497'
+offset = 0
+tracks = []
+
+user_info = sp.current_user()
+
+while True:
+    temp_tracks = sp.playlist_items(playlist_id, limit = 100, offset = offset)
+    #print(temp_tracks)
+    if len(temp_tracks) < 100:
+        tracks.append(temp_tracks)
+        break
+    else:
+        tracks.append(temp_tracks)
+        offset += 100
+
+
+for track in tracks[0]['items']:
+    print(track['track']['album']['name'],'\t\t\t\t',track['track']['album']['id'])
 song_id = "6Hmc5Afutf4hcrjnBYNidS?"
 colour = ["red", "blue", "yellow", "green", "purple"]
 
-sp.add_to_queue(song_id, ((sp.devices()).get('devices')[0]).get('id'))
-sp.next_track()
-print(sp.playlist_tracks("https://open.spotify.com/playlist/0urovHBAaGIhu1HNTcb1mv?si=18503fc027494715", "spotify", 1))    
+#sp.add_to_queue(song_id, ((sp.devices()).get('devices')[0]).get('id'))
+#sp.next_track()
+
 
 
 # results = sp.current_user_saved_tracks(10, 0)
